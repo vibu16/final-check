6a8052f2fd4e582aed5914f6d18edf7dfbcb797c
vibu16 <wb160499@gmail.com>
2019-12-07 15:22:52 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,31 @@
 // Include form validation functions here
+function validation() {
+		var movieTitle = document.forms["form"]["title"].value;
+		if(movieTitle== "") {
+		alert("Title is Required");
+		return false;
+		}
+		if ((movieTitle.length < 2) || (movieTitle.length > 100)) {
+        alert("Title should have 2 to 100 characters.");
+        return false;
+		}
+		var movieGross = document.forms["form"]["boxOffice"].value;
+		if(movieGross== "") {
+        alert("Gross is required.");
+		return false;
+		}
+		if(/[^0-9]/.test(movieGross)) {
+        alert("Gross has to be a number.");
+		return false;
+		}
+		var launchDate = document.forms["form"]["dateOfLaunch"].value;
+		if(launchDate== "") {
+        alert("Date of Launch is required.");
+		return false;
+		}
+		var movieGenre = document.forms["form"]["genre"].value;
+		if(movieGenre== "") {
+        alert("Genere is required.");
+        return false;
+		}
+		}
