6cd9787f476255740361ef02cd3c98d4f93fc26f
vibu16 <wb160499@gmail.com>
2020-01-10 14:11:18 +0530

    SQL

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,92 @@
--- Include table data insertion, updation, deletion and select scripts
+-- -----------------------------------------------------
+-- Adding Movie in Movie List Item
+-- -----------------------------------------------------
+INSERT INTO movie_list
+VALUES 
+(1, "Avatar", 2787965087, "Yes",  '2017/03/15',"Science Fiction", "Yes"),
+(2, "The Avengers", 1518812988, "Yes", '2017/12/23', "Superhero", "No"),
+(3, "Titanic", 2187463944, "Yes", '2018/08/21', "Romance", "No"),
+(4, "Jurassic World", 1671713208, "No", '2017/07/02',"Science Fiction", "Yes"),
+(5, "Avengers: End Game", 2750760348, "Yes", '2022/11/02',"Superhero", "Yes");
+
+-- -----------------------------------------------------
+-- Displaying Movies in movie_list Table
+-- -----------------------------------------------------
+SELECT * FROM movie_list;
+
+-- -----------------------------------------------------
+-- Updating Movie in Movie List Item
+-- -----------------------------------------------------
+
+UPDATE movie_list
+SET mo_title="Ben 10",mo_box_office=123456789,mo_active="Yes",mo_date_of_launch='2017/07/02',mo_genre="Action",mo_has_teaser="No"
+WHERE mo_id = 1;
+
+-- -----------------------------------------------------
+-- Displaying Movies in movie_list Table After Update
+-- -----------------------------------------------------
+SELECT * FROM movie_list;
+
+-- -----------------------------------------------------
+-- Adding User in User Table
+-- -----------------------------------------------------
+
+INSERT INTO user(us_name)
+VALUES('Ben'),('Sarath');
+
+-- -----------------------------------------------------
+-- Displaying User in User Table
+-- -----------------------------------------------------
+select * from user;
+
+-- -----------------------------------------------------
+-- Displaying the movie_list for User
+-- -----------------------------------------------------
+ 
+ select  mo_title, mo_box_office, mo_genre, mo_has_teaser from movie_list 
+ where mo_active='Yes' and mo_date_of_launch  <= (select(curdate()));
+ 
+-- -----------------------------------------------------
+-- Adding Movie into Favorites
+-- -----------------------------------------------------
+
+ insert into favorites(ft_us_id,ft_pr_id) values (1,2);
+ insert into favorites(ft_us_id,ft_pr_id) values (1,1);
+ insert into favorites(ft_us_id,ft_pr_id) values (2,1);
+ insert into favorites(ft_us_id,ft_pr_id) values (2,3);
+
+-- -----------------------------------------------------
+-- View Favorites
+-- -----------------------------------------------------
+
+select ft_id,mo_title,mo_box_office, mo_genre  from movie_list 
+inner join favorites on ft_pr_id=mo_id
+where ft_us_id=2;
+
+-- -----------------------------------------------------
+-- View No Of Favorites
+-- -----------------------------------------------------
+select count(mo_title) as No_of_favorites from movie_list
+inner join favorites on ft_pr_id=mo_id
+where ft_us_id=2;
+
+-- -----------------------------------------------------
+-- Remove Item From Favorites
+-- -----------------------------------------------------
+
+delete from favorites where ft_us_id=2 and ft_pr_id=3 limit 1;
+
+-- -----------------------------------------------------
+-- View Favorites After Delete
+-- -----------------------------------------------------
+
+select ft_id,mo_title,mo_box_office, mo_genre  from movie_list 
+inner join favorites on ft_pr_id=mo_id
+where ft_us_id=2;
+
+-- -----------------------------------------------------
+-- View No Of Favorites After Delete
+-- -----------------------------------------------------
+select count(mo_title) as No_of_favorites from movie_list
+inner join favorites on ft_pr_id=mo_id
+where ft_us_id=2;
--- sql/schema-creation.sql
+++ sql/schema-creation.sql
@@ -1,3 +1,61 @@
 -- 1. Design the schema for movie cruiser in MySQL Workbench
 -- 2. Generate the SQL schema script in MySQL Workbench
 -- 3. Paste the generated the SQL code here
+ 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+ 
+CREATE SCHEMA IF NOT EXISTS `moviecruiser` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `moviecruiser` ;
+ 
+-- -----------------------------------------------------
+-- Table `moviecruiser`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `moviecruiser`.`user` (
+  `us_id` INT NOT NULL AUTO_INCREMENT,
+  `us_name` VARCHAR(60) NULL,
+  PRIMARY KEY (`us_id`))
+ENGINE = InnoDB;
+ 
+-- -----------------------------------------------------
+-- Table `moviecruiser`.`movie_list`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `moviecruiser`.`movie_list` (
+  `mo_id` INT NOT NULL AUTO_INCREMENT,
+  `mo_title` VARCHAR(100) NULL,
+  `mo_box_office` INT(15) NULL,
+  `mo_active` VARCHAR(3) NULL,
+  `mo_date_of_launch` DATE NULL,
+  `mo_genre` VARCHAR(45) NULL,
+  `mo_has_teaser` VARCHAR(3) NULL,
+  PRIMARY KEY (`mo_id`))
+ENGINE = InnoDB;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `moviecruiser`.`favorites`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `moviecruiser`.`favorites` (
+  `ft_id` INT NOT NULL AUTO_INCREMENT,
+  `ft_us_id` INT NULL,
+  `ft_pr_id` INT NULL,
+  PRIMARY KEY (`ft_id`),
+  INDEX `ft_us_fk_idx` (`ft_us_id` ASC),
+  INDEX `ft_pr_fk_idx` (`ft_pr_id` ASC),
+  CONSTRAINT `ft_us_fk`
+    FOREIGN KEY (`ft_us_id`)
+    REFERENCES `moviecruiser`.`user` (`us_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `ft_pr_fk`
+    FOREIGN KEY (`ft_pr_id`)
+    REFERENCES `moviecruiser`.`movie_list` (`mo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+ 
+ 
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
